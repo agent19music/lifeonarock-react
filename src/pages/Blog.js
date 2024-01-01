ac49aa59f42bd00fed7f1df10d215042d8ae7508
agent19music <seanmotanya@gmail.com>
2024-01-01 12:04:51 +0300

    Update blogs.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Swal from 'sweetalert2'
 
-export default function Blog() {
+export default function Blog(blogs) {
+  function deleteBlog (){
+    fetch('',{
+      method: 'DELETE'
+    })
+    .then((res)=>(res.json()))
+    .then ((res)=>{
+      console.log(res)
+      Swal.fire(
+        'Blog Deleted!',
+        '',
+        'success'
+      )
+    })
+  }
   return (
-    <div>Blog</div>
+    <div className='col-md-3 mb-5'>
+       <img src={blogs.poster} className='img-fluid'alt='loading'/>
+        <div>
+        <h6>{blogs.title}</h6>
+        </div>  
+    </div>
   )
 }
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import Blog from './Blog'
 
 export default function Blogs() {
+  const [blogs, setBlogs] = useState({})
+  useEffect(()=>{
+    fetch ('http://localhost:6001/blogs')
+    .then((res) => res.json())
+    .then((res)=> setBlogs(res) )
+  },[])
   return (
-    <div>Blogs</div>
+    <div className='container p-3'>
+      <h2>Blogs</h2>
+      {blogs.length <1 && <p>'Sorry. No blogs available today :3</p>}
+    <div className='co'>
+      {
+        blogs && blogs.map((blog)=>(
+          <Blog key ={blog.id} blogs={blog}/>
+        ))
+      }
+      </div>  
+
+    </div>
   )
 }
