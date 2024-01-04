09425012dc738d62e81c14707ff19dc3ae175964
agent19music <seanmotanya@gmail.com>
2024-01-04 14:52:04 +0300

    Update blog info

--- src/pages/Bloginfo.js
+++ src/pages/Bloginfo.js
@@ -5,13 +5,42 @@ export default function Bloginfo() {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
 
+  const [isLiked, setIsLiked] = useState([]) 
+  const toggleLike = () => {
+   setIsLiked(!isLiked);
+ };
+  const like = isLiked ? 'fa-heart':'fa-heart.liked';
+
   useEffect(() => {
     fetch(`http://localhost:6001/blogs/${id}`)
       .then((res) => res.json())
-      .then((res) => setBlog(res))
+      .then((res) => {
+        // Add a 'liked' property to each comment
+        // const updatedComments = res.comments.map(comment => ({ ...comment, liked: false }));
+        // setBlog({ ...res, comments: updatedComments });
+        setBlog(res)
+      })
       .catch((error) => console.error(error));
 
   }, [id]);
+
+  const handleLike = (commentId) => {
+    // Find the comment and toggle its 'liked' status
+    // const updatedComments = blog.comments.map(comment => {
+    //   if (comment.id === commentId) {
+    //     comment.liked = !comment.liked;
+    //     // Send a PATCH request to update the like count
+    //     // fetch(`http://localhost:6001/comments/${commentId}`, {
+    //     //   method: 'PATCH',
+    //     //   headers: { 'Content-Type': 'application/json' },
+    //     //   body: JSON.stringify({ likes: comment.liked ? comment.likes + 1 : comment.likes - 1 }),
+    //     // }).catch((error) => console.error(error));
+    //   }
+    //   return comment;
+    // });
+    // setBlog({ ...blog, comments: updatedComments });
+
+  };
 
   return (
     <div id='content' className="container mt-4">
@@ -34,7 +63,7 @@ export default function Bloginfo() {
                   <div className="card-body">
                     <h6 className="card-title">{comment.username}</h6>
                     <p className="card-text">{comment.comment}</p>
-                    <p className="card-text">Likes: {comment.likes}</p>
+                    <p className="card-text">Likes: {comment.likes} <i id={like} className={`fas fa-regular fa-heart`} onClick={()=>toggleLike()}></i></p>
                   </div>
                 </div>
               ))}
