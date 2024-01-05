3fd8931e3b7c7f375b3f747c81fb9935b691f90d
agent19music <seanmotanya@gmail.com>
2024-01-05 12:54:21 +0300

    Update pad drive

--- src/pages/Paddrive.js
+++ src/pages/Paddrive.js
@@ -4,12 +4,12 @@ import Swal from 'sweetalert2';
 export default function Paddrive({toggle, toggle3}) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [thoughts, setThoughts] = useState('');
+  const [donation, setDonation] = useState('');
   const [rating, setRating] = useState(0);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setThoughts('');
+    setDonation('');
     setName('');
     setEmail('');
     setRating(0);
@@ -22,7 +22,7 @@ export default function Paddrive({toggle, toggle3}) {
     });
   };
   return (
-    <div>
+    <div id=''>
       <h3>Donate to the Pad Drive</h3>
       <h4>Donation Details</h4> 
       <h6>Fill out your donation information</h6>
@@ -32,7 +32,7 @@ export default function Paddrive({toggle, toggle3}) {
             <form onSubmit={handleSubmit} className={`${toggle} p-5 my-5 bg-light rounded`}>
               <div className="mb-3">
                 <label htmlFor="name" className={`form-label text-${toggle3}`}>
-                  Name:
+                  Full Name:
                 </label>
                 <input
                   type="text"
@@ -45,7 +45,55 @@ export default function Paddrive({toggle, toggle3}) {
               </div>
               <div className="mb-3">
                 <label htmlFor="email" className={`form-label text-${toggle3}`}>
-                  Email:
+                  Email Address:
+                </label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  id="email"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="donation" className={`form-label text-${toggle3}`}>
+                  Donation amount:
+                </label>
+                <input
+                  type="number"
+                  value={donation}
+                  onChange={(event) => setDonation(event.target.value)}
+                  id="donation"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className={`form-label text-${toggle3}`}>
+                  Payment Method:
+                </label>
+                <input
+                  type="radio button"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  id="email"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className={`form-label text-${toggle3}`}>
+                 Country:
+                </label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  id="email"
+                  className="form-control"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className={`form-label text-${toggle3}`}>
+                 Donate monthly?:
                 </label>
                 <input
                   type="email"
@@ -58,7 +106,7 @@ export default function Paddrive({toggle, toggle3}) {
               
 
               <button type="submit" className="btn btn-success btn-md">
-                Send Feedback
+                Donate
               </button>
             </form>
           </div>
