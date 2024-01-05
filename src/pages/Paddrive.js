2af0212baf1464b5429f98a40bd7d42a4a501f53
agent19music <seanmotanya@gmail.com>
2024-01-05 12:19:20 +0300

    Add pad drive page

---
+++ src/pages/Paddrive.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Paddrive() {
+  return (
+    <div>
+      <h3>Donate to the Pad Drive</h3>
+      <h4>Donation Details</h4> 
+      <h6>Fill out your donation information</h6>
+      
+    </div>
+  )
+}
