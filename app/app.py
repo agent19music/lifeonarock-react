1cf47641f4fffba23026654f7c22bed8afa0ec23
agent19music <seanmotanya@gmail.com>
2024-01-16 14:06:03 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 
-from models import db
+from models import db,Blog
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -17,6 +17,21 @@ db.init_app(app)
 @app.route('/')
 def home():
     return''
+
+@app.route('/blogs', methods ='GET')
+def get_all_blogs():
+    blogs = []
+    for blog in Blog.query.all():
+        blog_dict = {
+            "id":blog.id,
+            "title":blog.title,
+            'content': blog.content,
+            'likes': blog.likes,
+            'comments': blog.comments
+        }
+        blogs.append(blog_dict)
+    return make_response(jsonify(blogs),200)         
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
