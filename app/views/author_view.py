04a048961e7d1f1f885fad996725a079d4df0446
agent19music <seanmotanya@gmail.com>
2024-01-17 21:51:03 +0300

    Update author_view.py

--- app/views/author_view.py
+++ app/views/author_view.py
@@ -4,7 +4,67 @@ from sqlalchemy.orm import joinedload
 
 authour_bp = Blueprint('author_bp', __name__)
 
+#add author
 @authour_bp.route('/author', methods=['POST'])
-def create_answer():
-    data = request.json
-    new_answer = Author(name=data['body'],)
+def create_author():
+    data = request.get_json()
+    new_author = Author(id=data['id'],name=data['name'],email=data['email'])
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return jsonify({'message':'Author added successfully'})
+
+#get all authors
+@authour_bp.route('/authors')
+def get_authors():
+    authors = Author.query.all()
+    author_list = []
+    for author in authors:
+        author_list.append({
+            'id':author.id,
+            'email': author.email,
+            'name': author.name
+        })
+    return jsonify(author_list), 200  
+
+#fetch single author
+@authour_bp.route('/authors/<int:id>')
+def get_user(id):
+    author = Author.query.filter_by(id==id).first()
+    author_list = []
+
+    if author:
+        author_list.append({
+            'id' : author.id,
+            'email': author.email,
+            'name' : author.name
+        })
+        return jsonify(author_list), 200
+    
+    else:
+        return jsonify({"error":"User not found!"}), 404
+
+#update user
+@authour_bp.route('/authors/<int:id>', methods=['PUT'])
+def update_user(id):
+    author = Author.query.get(id)
+    if author:
+        name = request.form.get('name')
+        email = request.form.get('email')
+
+        check_email = Author.query.filter_by(email=email).first()
+        check_name = Author.query.filter_by(name=name).first()
+        
+        if check_email or check_email or check_name:
+            return jsonify({"error": "Author email/name already exist!"})
+        
+        else:
+            author.name = name
+            author.email = email
+        
+            db.session.commit()
+            return jsonify({"success": "Author updated successfully"}), 200
+
+    else:
+        return jsonify({"error":"Author you are trying to update doesn't exist!"}), 404
