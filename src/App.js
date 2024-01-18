9eedea3cf0961c8f71e191ba91df70436e6aa49c
agent19music <seanmotanya@gmail.com>
2024-01-18 22:57:20 +0300

    Update usercontext

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Footer from './layout/Footer.js';
 import Blogs from './pages/Blogs.js';
 import Bloginfo from './pages/Bloginfo.js';
 import Paddrive from './pages/Paddrive.js';
+import BlogProvider from './context/BlogContext.js';
 
 
 
@@ -36,6 +37,7 @@ useEffect(()=>{
 console.log(blogs)
   return (
     <div className="App">
+<BlogProvider>
 <BrowserRouter>
 <Navbar/>
   <Routes>  
@@ -49,13 +51,10 @@ console.log(blogs)
           <Route path="/blogs" element={<Blogs blogs={blogs}/>} />
           <Route path="/blogs/:id" element={<Bloginfo blogs={blogs}/>} />
           <Route path="/paddrive" element={<Paddrive toggle={toggle} toggle3={toggle3} />} />
-          
-
-        
-          
      </Routes> 
      <Footer/>
      </BrowserRouter>
+     </BlogProvider>
     </div>
   );
 }
