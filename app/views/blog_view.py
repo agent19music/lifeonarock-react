1f8c503aed9a9c335871389c2b4dc02d2782af90
agent19music <seanmotanya@gmail.com>
2024-01-18 21:10:04 +0300

    Update contexts

--- app/views/blog_view.py
+++ app/views/blog_view.py
@@ -8,40 +8,45 @@ blog_bp = Blueprint('blog_bp', __name__)
 # Fetch all blogs
 @blog_bp.route('/blogs', methods=['GET'])
 def get_all_blogs():
-    blogs =  Blog.query.all()
- 
-    result = []
-    for blog in blogs:
-        comments = blog.comments
+    blogs = []
+    for blog in Blog.query.all():
+        blog_dict = {
+            "id": blog.id,
+            "title": blog.title,
+            'content': blog.content,
+            'likes': blog.likes,
+            'comments': []
+        }
 
-        result.append({
+        for comment in blog.comments:
+            comment_dict = {
+                "id": comment.id,
+                "content": comment.content,
+                "likes": comment.likes,
+                "user_id": comment.user_id,  # Add other relevant fields
+            }
+            blog_dict['comments'].append(comment_dict)
+
+        blogs.append(blog_dict)
+
+    return jsonify(blogs), 200
+
+# Fetch a single Question
+@blog_bp.route('/blogs/<int:blog_id>', methods=['GET'])
+def get_blog(blog_id):
+    blog = Blog.query.get(blog_id)
+
+    if blog:
+        result = {
             'id': blog.id,
             'title': blog.title,
             'content': blog.content,
             'likes': blog.likes,
-            'author': {
-                'id': blog.author.id if blog.author else None,
-                'name': blog.author.username if blog.author else None
-            },
-            'comments': [{'id': comment.id, 'content': comment.content} for comment in comments]
-        })
-    return jsonify(result), 200
-
-# Fetch a single Question
-@blog_bp.route('/blogs/<int:blog_id>', methods=['GET'])
-def get_question(blog_id):
-    blog = Blog.query.get(blog_id)
-    result = []
-
-    if blog:
-        result.append({'id': blog.id, 'title': blog.title, 
-                'content': blog.content, 
-                'likes': blog.likes, 
-                'author': {'id': blog.author.id, 'name': blog.author.name },
-                'comments': [{'id': comment.id, 'content': comment.content} for comment in blog.comments]
-
-                       })
+            'author': {'id': blog.author.id, 'name': blog.author.name},
+            'comments': [{'id': comment.id, 'content': comment.content} for comment in blog.comments]
+        }
         return jsonify(result)
-    return jsonify({"error": "Question not found!"}), 404
+
+    return jsonify({"error": "Blog not found!"}), 404
 
 
