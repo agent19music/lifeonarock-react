ab0ebda551f131ea98d82fe83b8f25074f2eed83
agent19music <seanmotanya@gmail.com>
2024-01-19 19:13:34 +0300

    Update auth view

--- app/views/auth_view.py
+++ app/views/auth_view.py
@@ -3,3 +3,23 @@ from flask import request, jsonify, Blueprint
 from werkzeug.security import check_password_hash
 
 auth_bp = Blueprint('auth_bp', __name__)
+
+# routes
+# add user
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    
+    user = User.query.filter_by(username = username).first()
+
+    if user:
+        if check_password_hash(user.password, password):
+            access_token = create_access_token(identity=user.id)
+            return jsonify(access_token = access_token)
+        
+        return jsonify({"error": "Wrong Password!"}), 401
+
+    else:
+        return jsonify({"error": "User doesn't exist!"}), 404
