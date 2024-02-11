d276d82bcec3a8d1ec29b655c8b9249d2685ff9e
agent19music <seanmotanya@gmail.com>
2024-02-11 13:10:29 +0300

    Update blog_view

--- app/views/blog_view.py
+++ app/views/blog_view.py
@@ -1,1 +1,9 @@
 from models import db, Blog
+from flask import request, jsonify, Blueprint
+
+blog_bp = Blueprint('blog_bp', __name__)
+
+@blog_bp.route('/blogs', methods=['GET'])
+def get_all_blogs():
+    blogs = [blog.to_dict() for blog in db.session.query(Blog).all()]
+    return jsonify(blogs), 200
