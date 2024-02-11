8b32306ff2e29e532bf0970d0f6d3e51ec7e0255
agent19music <seanmotanya@gmail.com>
2024-02-11 13:28:18 +0300

    Add comment_view

--- app/app.py
+++ app/app.py
@@ -3,25 +3,22 @@
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from datetime import timedelta
+from views import *
 
 
-from models import db,Blog
+from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-Migrate(app, db)
+
+migarate = Migrate(app, db)
 
 db.init_app(app)
 
-
-
-@app.route('/')
-def home():
-    return''
-
+app.register_blueprint(blog_bp)
 
 
 if __name__ == '__main__':
