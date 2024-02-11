f32e7f7413912fc1d4672c9e4b3fa70e03e5f635
agent19music <seanmotanya@gmail.com>
2024-02-11 10:42:21 +0300

    Restructure the jwt , get rid of it for now

--- app/app.py
+++ app/app.py
@@ -6,8 +6,6 @@ from datetime import timedelta
 
 
 from models import db,Blog
-from views import *
-from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -17,23 +15,7 @@ app.json.compact = False
 Migrate(app, db)
 
 db.init_app(app)
-jwt = JWTManager()
-app.config["JWT_SECRET_KEY"] = "fjhjdjhfiskyfvdgvydklvsrfl"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
-jwt.init_app(app)
 
-app.register_blueprint(user_bp)
-app.register_blueprint(blog_bp)
-app.register_blueprint(authour_bp)
-app.register_blueprint(auth_bp)
-app.register_blueprint(comment_bp)
-
-# JWT LOADER
-@jwt.token_in_blocklist_loader
-def token_in_blocklist_callback(jwt_header, jwt_data):
-    jti = jwt_data['jti']
-    token = TokenBlocklist.query.filter_by(jti=jti).first()
-    return token is not None
 
 
 @app.route('/')
