104005ce6078d274db31b531362c7e82e8edd6d9
agent19music <seanmotanya@gmail.com>
2024-02-12 14:59:57 +0300

    Add get single blog route

--- app/views/blog_view.py
+++ app/views/blog_view.py
@@ -5,5 +5,15 @@ blog_bp = Blueprint('blog_bp', __name__)
 
 @blog_bp.route('/blogs', methods=['GET'])
 def get_all_blogs():
-    blogs = [ {"id": blog.id, "poster": blog.poster, "title": blog.title,"content": blog.content,"likes": blog.likes} for blog in db.session.query(Blog).all()]
+    blogs = [ {"id": blog.id, "poster": blog.poster, 
+            "title": blog.title,"content": blog.content,
+            "likes": blog.likes} for blog in db.session.query(Blog).all()]
     return jsonify(blogs), 200
+
+@blog_bp.route('/blogs/<int:id>',methods=['GET'])
+def get_blog(id):
+    blog = db.session.query(Blog).filter_by(id=id).first()
+    if blog:
+        return jsonify(blog.to_dict()), 200
+    else:
+        return jsonify({"error": "Blog not found"}), 404
