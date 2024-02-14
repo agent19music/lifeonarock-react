29e98ae984d416aa2fd1e976bd9c72c82107dc72
agent19music <seanmotanya@gmail.com>
2024-02-14 21:43:34 +0300

    Update About us

--- client/src/pages/Aboutus.js
+++ client/src/pages/Aboutus.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Aboutus() {
   return (
-    <div id='page'>Aboutus</div>
+    <div id='page'>
+      Young 
+      Rich
+      Black
+      Horny
+    </div>
   )
 }
